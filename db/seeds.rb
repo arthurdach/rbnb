486330b906256907e0bbdf9225b98655998f5de0
Thibaut ANDRY <andrythibaut@gmail.com>
2018-11-26 17:32:04 +0100

    seed en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u = User.create(name: 'Arthur', email: 'arthur@sport-rbnb.com', address: '16 villa Gaudelet Paris 75011')
+m = Material.create(user: 'u', description: 'Ski Salomon x-Stream en excellent état', available: false)
+l = Location.create(type: 't', start_date: date.taday, end_date: date.today + 7.day, )
+t = Type.create(name: 'SKI')
+r = Reviews.create(content: 'excellente location de ski' , rating: '5', location: 'l')
+
+
+u1 = User.create(name: 'Martin', email: 'martin@sport-rbnb.com', address: '69 rue de la Pompe Paris 75016')
