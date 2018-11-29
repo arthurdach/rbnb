a772f08e280cff03088cf7b49df686fa9a70cb29
Thibaut ANDRY <andrythibaut@gmail.com>
2018-11-29 16:22:27 +0100

    modif pour ajout prix, seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_105631) do
+ActiveRecord::Schema.define(version: 2018_11_29_103010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_105631) do
     t.string "name"
     t.string "category"
     t.string "photo"
+    t.float "price"
     t.index ["user_id"], name: "index_materials_on_user_id"
   end
 
