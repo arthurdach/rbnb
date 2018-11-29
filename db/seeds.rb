d54261dee7c6688e2abbc7211e793edab050598a
Thibaut ANDRY <andrythibaut@gmail.com>
2018-11-29 18:31:18 +0100

    commit a effacer

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,7 +41,7 @@ CATEGORIES = %w[ski surf kite velo snowboard skateboard paddle].freeze
 matOne = Material.new(
   name: "Surf factory évolutive 7.1",
   description: "Planche de surf de la marque Surf Factory. Taille 7.1, idéal pour débutant. Très bon état général, dispo à domicile",
-  available = true,
+  available: true,
   category: "surf",
   price: "16.00"
 )
@@ -53,7 +53,7 @@ matOne.save!
 matTwo = Material.new(
   name: "Surf Mini Malibu 8.6",
   description: "Surf 8.6 idéal pour expert. Très bon état général, dispo à domicile. Possibilité de me déplacer en région parisienne sur demande.",
-  available = true,
+  available: true,
   category: "surf",
   price: "17.00"
 )
@@ -66,7 +66,7 @@ matTwo.save!
 matThree = Material.new(
   name: "Skis Salomon 2010",
   description: "Skis utilisés environ 2 mois. Comme neuf. Visible sur demande sur Paris si besoin. Possibilité de conseils si nécessaire. Bon ride!",
-  available = true,
+  available: true,
   category: "ski",
   price: "18.00"
 )
@@ -78,7 +78,7 @@ matThree.save!
 matFour = Material.new(
   name: "Skateboard Princess",
   description: "Skateboard en bon état ayant servi environ 50h. Très bon état général, dispo à domicile. Equippement de protection égaelement dispo si besoin.",
-  available = true,
+  available: true,
   category: "skateboard",
   price: "19.00"
 )
@@ -121,45 +121,60 @@ locationTwo.save!
 
 
 review1 = MaterialReview.new(
-  rating: 5
-  description: "Excellent matos, et très bon état, proprio au top"
-  material_id: 1
+  rating: 5,
+  description: "Excellent matos, et très bon état, proprio au top",
   )
+  review1.material = matOne
+  matOne.save
+
 review2 = MaterialReview.new(
-  rating: 4
-  description: "Matériel en bon état d'usage, en revanche voir a affuter les ski, propiétaire charmante"
-  material_id: 1
+  rating: 4,
+  description: "Matériel en bon état d'usage, en revanche voir a affuter les ski, propiétaire charmante",
   )
+  review2.material = matOne
+  matOne.save
+
 review3 = MaterialReview.new(
-  rating: 3
-  description: "Un peu déçu par ces skis, il manque de rigidité, pour debutant. Le propriétaire avait oublié de me le dire"
-  material_id: 2
+  rating: 3,
+  description: "Un peu déçu par ces skis, il manque de rigidité, pour debutant. Le propriétaire avait oublié de me le dire",
   )
+  review3.material = matTwo
+  matTwo.save
+
 review4 = MaterialReview.new(
-  rating: 5
-  description: "Très bon surf, la neige en plus était au top, l'expertise de son propriétaire m'a été utile. Un grand merci je reviendrai"
-  material_id: 2
+  rating: 5,
+  description: "Très bon surf, la neige en plus était au top, l'expertise de son propriétaire m'a été utile. Un grand merci je reviendrai",
   )
+  review4.material = matTwo
+  matTwo.save
+
 review5 = MaterialReview.new(
-  rating: 2
-  description: "Matos en état d'usure assez avancé !! dommage !! cela gâche tout"
-  material_id: 3
+  rating: 2,
+  description: "Matos en état d'usure assez avancé !! dommage !! cela gâche tout",
   )
+  review5.material = matThree
+  matThree.save
+
 review6 = MaterialReview.new(
-  rating: 1
-  description: "Horrible !!!  le propietaire n'est pas du tout sympa et son matériel est hors d'usage !!! A FUIR !!!!!"
-  material_id: 3
+  rating: 1,
+  description: "Horrible !!!  le propietaire n'est pas du tout sympa et son matériel est hors d'usage !!! A FUIR !!!!!",
   )
+  review6.material = matThree
+  matThree.save
+
 review7 = MaterialReview.new(
-  rating: 4
-  description: "Excellent matos, la propriétaire est de plus très agréable"
-  material_id: 4
+  rating: 4,
+  description: "Excellent matos, la propriétaire est de plus très agréable",
   )
+  review7.material = matFour
+  matFour.save
+
 review8 = MaterialReview.new(
-  rating: 5
-  description: "Très content de ma location, je suis très content, A RECOMMANDER les yeux fermés."
-  material_id: 4
+  rating: 5,
+  description: "Très content de ma location, je suis très content, A RECOMMANDER les yeux fermés.",
   )
+  review8.material = matFour
+  matFour.save
 
 
 
