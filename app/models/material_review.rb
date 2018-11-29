5127387548b3b97b1b69aee0e71efecace5d635d
arthur d achon <arthur.dachon@gmail.com>
2018-11-29 15:40:54 +0100

    Review table + Controller + Routes + Pundit + Views

--- app/models/material_review.rb
+++ app/models/material_review.rb
@@ -1,3 +1,8 @@
 class MaterialReview < ApplicationRecord
+  RATINGS = [0, 1, 2, 3, 4, 5].freeze
   belongs_to :material
+  validates :rating, inclusion: {
+    in: RATINGS,
+    message: "Veuillez noter de 0 à 5"
+  }
 end
