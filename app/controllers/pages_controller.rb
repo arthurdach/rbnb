8fcd5dba195ebe2187de8cbfc5191c503344c32f
Thibaut ANDRY <andrythibaut@gmail.com>
2018-11-30 14:50:22 +0100

    map avec les cartes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,13 @@ class PagesController < ApplicationController
       sql_query = "materials.category ILIKE ? AND users.address ILIKE ?"
       @materials = Material.joins(:user).where(sql_query, "%#{params[:category]}%", "%#{params[:address]}%")
       @markers = []
+      @query = true
       @materials.each do |material|
         user = material.user
         @markers << { lat: user.latitude, lng: user.longitude }
       end
     else
+      @query = false
       @materials = Material.all
     end
 
